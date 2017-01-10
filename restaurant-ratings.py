56172dff4b9257bd167182ec32983c2bce67dee7
Erika Azabache <erika@doravel.me>
2017-01-09 17:26:33 -0800

    Displays a menu with 3 options to either show the restaurants ordered alphabetically, add a new restaurant and rate it, or quit

--- restaurant-ratings.py
+++ restaurant-ratings.py
@@ -1,5 +1,5 @@
 # your code goes here
-
+import sys
 
 def ratings_dictionary(filename):
     restaurant_rating_file = open(filename)
@@ -15,9 +15,23 @@ def print_ratings(ratings_dictionary):
     for restaurant_name, restaurant_rating in sorted(ratings_dictionary.items()):
         print "%s is rated at %s" % (restaurant_name, restaurant_rating)
 
-restaurant_rating_dict = ratings_dictionary("scores.txt")
-new_restaurant = raw_input("Enter new restaurant:")
-new_rating = raw_input("Enter rating for new restaurant:")
+while True:
+    print "\nHi! please enter 1 to see all ratings in alphabetical order."
+    print "Or, please enter 2 to add a new restaurant and rating it."
+    print "Or, please enter 3 to quit."
 
-restaurant_rating_dict[new_restaurant] = new_rating
-print_ratings(restaurant_rating_dict)
+    choice = raw_input()
+
+    if choice == '1':
+        restaurant_rating_dict = ratings_dictionary("scores.txt")
+        print_ratings(restaurant_rating_dict)
+    elif choice == '2':
+        restaurant_rating_dict = ratings_dictionary("scores.txt")
+        new_restaurant = raw_input("Enter new restaurant:")
+        new_rating = raw_input("Enter rating for new restaurant:")
+        restaurant_rating_dict[new_restaurant] = new_rating
+        print_ratings(restaurant_rating_dict)
+    elif choice == '3':
+        sys.exit(0)
+    else:
+        print "Enter one of the given options, please."

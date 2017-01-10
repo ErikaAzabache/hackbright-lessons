2a7036653a18c91a36cb20cd53ca74dc6183bcc2
Erika Azabache <erika@doravel.me>
2017-01-09 17:45:45 -0800

    Option 4 added. It Updates the rating of a random restaurant.

--- restaurant-ratings.py
+++ restaurant-ratings.py
@@ -1,5 +1,6 @@
 # your code goes here
 import sys
+import random
 
 def ratings_dictionary(filename):
     restaurant_rating_file = open(filename)
@@ -15,23 +16,29 @@ def print_ratings(ratings_dictionary):
     for restaurant_name, restaurant_rating in sorted(ratings_dictionary.items()):
         print "%s is rated at %s" % (restaurant_name, restaurant_rating)
 
+restaurant_rating_dict = ratings_dictionary("scores.txt")
 while True:
     print "\nHi! please enter 1 to see all ratings in alphabetical order."
     print "Or, please enter 2 to add a new restaurant and rating it."
-    print "Or, please enter 3 to quit."
+    print "Or, please enter 3 to update a restaurant's rating."
+    print "Or, please enter 4 to quit."
 
     choice = raw_input()
 
     if choice == '1':
-        restaurant_rating_dict = ratings_dictionary("scores.txt")
         print_ratings(restaurant_rating_dict)
     elif choice == '2':
-        restaurant_rating_dict = ratings_dictionary("scores.txt")
         new_restaurant = raw_input("Enter new restaurant:")
         new_rating = raw_input("Enter rating for new restaurant:")
         restaurant_rating_dict[new_restaurant] = new_rating
         print_ratings(restaurant_rating_dict)
     elif choice == '3':
+        random_restaurant = random.choice(restaurant_rating_dict.keys())
+        print "Please rate %s: " % random_restaurant
+        new_rating = raw_input()
+        restaurant_rating_dict[random_restaurant] = new_rating
+        print_ratings(restaurant_rating_dict)
+    elif choice == '4':
         sys.exit(0)
     else:
         print "Enter one of the given options, please."

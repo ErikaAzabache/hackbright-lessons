4e6b0b5846a1479e2e7bcdcef7160107294e6a57
Erika Azabache <erika@doravel.me>
2017-01-27 13:00:42 -0800

    Ability to order melons

--- static/ajax-exercise.js
+++ static/ajax-exercise.js
@@ -37,11 +37,32 @@ $("#weather-form").on('submit', showWeather);
 
 // PART 3: ORDER MELONS
 
+function alertOrder(results) {
+    var code = results['code'];
+    var msg = results['msg'];
+    $('#order-status').html(msg);
+
+    if (code == "ERROR") {
+        $('#order-status').addClass('order-error');
+    } else {
+        $('#order-status').removeClass('order-error');
+    }
+}
+
+
 function orderMelons(evt) {
     evt.preventDefault();
 
     // TODO: show the result message after your form
     // TODO: if the result code is ERROR, make it show up in red (see our CSS!)
+
+    var formInputs = {
+        'melon_type': $('#melon-type-field').val(),
+        'qty': $('#qty-field').val()
+    };
+
+    $.post("/order-melons.json", formInputs, alertOrder);
+    
 }
 
 $("#order-form").on('submit', orderMelons);

eabd7719fcf2bb34a30c8a7d695ad49ff333f1ea
Erika Azabache <erika@doravel.me>
2017-01-27 12:32:13 -0800

    Shows forecast

--- static/ajax-exercise.js
+++ static/ajax-exercise.js
@@ -23,6 +23,11 @@ function showWeather(evt) {
     var url = "/weather.json?zipcode=" + $("#zipcode-field").val();
 
     // TODO: request weather with that URL and show the forecast in #weather-info
+
+    $.get(url, function(results) {
+        var forecast = results['forecast'];
+        $("#weather-info").html(forecast);
+    })
 }
 
 $("#weather-form").on('submit', showWeather);
